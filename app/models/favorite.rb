2fd85fa3d8e7d93a7f15933c2d43719cdb44d5c9
sophie <sophiefishman@gmail.com>
2021-06-22 11:34:02 -0500

    adds associations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,18 @@
 class Favorite < ApplicationRecord
+  belongs_to :user
+  def movie
+    #logic to query the third party api for the correct movie
+    #get back an object of movie data
+    response = HTTP.get("https://api.themoviedb.org/3/movie/#{tmdb_api_id}?api_key=tmdb_api_key")
+    movie = response.parse(:json)
+    return movie
+  end
+    
+  #or build custom object below 
+  #   return{
+  #     id: movie["id"]
+  #     original_title: movie["original_title"]
+  #   }
+  # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create({name: "Ringo", email: "ringo@dog.com", password_digest: "kljadjoij23"})
+
+users = User.create({name: "Sunny", email: "sunny@dog.com", password_digest: "dsfad6565"})
+
+users = User.create({name: "Karma", email: "karam@dog.com", password_digest: "sadkjn75365@"})
+
+favorites = Favorite.create({tmdb_api_id: "550", user_id: 1, review: "Love this movie adaptation of the Chuck Palahniuk novel Fight Club!"})
+
+favorites = Favorite.create({tmdb_api_id: "254320", user_id: 3, review: "A weird dark romantic comedy, loved the symbolism of The Lobster"})
