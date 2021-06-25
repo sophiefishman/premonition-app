7d8279bb5810f832c2df4609f256f6d4a1ff7ae7
sophie <sophiefishman@gmail.com>
2021-06-25 15:49:12 -0500

    adds favorites serializer

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,12 +1,13 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   def movie
-    response = HTTP.get("https://api.themoviedb.org/3/movie/550?api_key=tmdb_api_key")
-    response.parse(:json)["results"][0]
+    response = HTTP.get("https://api.themoviedb.org/3/movie/#{tmdb_api_id}?api_key=#{Rails.application.credentials.tmdb_api_key}")
+    movie = response.parse(:json)
     return{
       id: movie["id"],
       original_title: movie["original_title"],
       overview: movie["overview"],
+      poster_path: movie["poster_path"],
     }
   end
 end
