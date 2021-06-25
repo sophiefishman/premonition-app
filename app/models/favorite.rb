d23bf48ea3887118695c15a822af9f29e171efe1
sophie <sophiefishman@gmail.com>
2021-06-24 20:35:43 -0500

    edits favorites custom response

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,13 +1,12 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  # def movie
-  #   response = HTTP.get("https://api.themoviedb.org/3/movie/550?api_key=tmdb_api_key")
-  #   movie = response.parse(:json)
-  #   return{
-  #     id: movie["id"]
-  #     original_title: movie["original_title"]
-  #     overview: movie["overview"]
-  #   }
-  # end
-
+  def movie
+    response = HTTP.get("https://api.themoviedb.org/3/movie/550?api_key=tmdb_api_key")
+    response.parse(:json)["results"][0]
+    return{
+      id: movie["id"],
+      original_title: movie["original_title"],
+      overview: movie["overview"],
+    }
+  end
 end
