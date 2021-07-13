619d54450b73914895e7269ffecc69be2f5bbc7b
sophie <sophiefishman@gmail.com>
2021-07-13 13:44:58 -0500

    adds order to favorites index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    favorites = current_user.favorites
+    favorites = current_user.favorites.order('created_at ASC')
     render json: favorites
   end
 
